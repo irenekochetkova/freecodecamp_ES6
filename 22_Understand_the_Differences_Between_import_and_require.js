7ede0a1f4898e06e63220c019f500f7483c4aa50
Irina <irenekochetkova@gmail.com>
2018-06-22 09:03:32 -0700

    added the solution of the task Understand the Differences Between import and require

---
+++ 22_Understand_the_Differences_Between_import_and_require.js
@@ -0,0 +1,10 @@
+// Add the appropriate import statement that will allow the current file to use the capitalizeString function. The file where this function lives is called "string_functions", and it is in the same directory as the current file
+
+// "use strict";
+// capitalizeString("hello!");
+// ==========================================
+
+import { capitalizeString } from "string_functions"
+
+"use strict";
+capitalizeString("hello!");
