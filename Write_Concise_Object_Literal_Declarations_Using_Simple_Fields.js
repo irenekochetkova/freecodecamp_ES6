9c03bead8de24e394081e984ee4e7f0b4f464d91
Irina <irenekochetkova@gmail.com>
2018-06-21 20:42:29 -0700

    added the solution of the task  Write Concise Object Literal Declarations Using Simple Fields

---
+++ Write_Concise_Object_Literal_Declarations_Using_Simple_Fields.js
@@ -0,0 +1,22 @@
+// Use simple fields with object literals to create and return a Person object.
+
+// const createPerson = (name, age, gender) => {
+//   "use strict";
+//   // change code below this line
+//   return {
+//     name: name,
+//     age: age,
+//     gender: gender
+//   };
+//   // change code above this line
+// };
+// console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
+// =============================================================================
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  ({name, age, gender})
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
